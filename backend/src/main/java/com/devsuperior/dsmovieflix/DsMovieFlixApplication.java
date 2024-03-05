c11c46618243273a993211dc6ded29f9d7cf3f4f
Clayton Pereira <clayton.pereira@outlook.com>
2024-03-04 21:23:46 -0300

    BackEnd Challenge MovieFlix - Project Created

---
+++ backend/src/main/java/com/devsuperior/dsmovieflix/DsMovieFlixApplication.java
@@ -0,0 +1,13 @@
+package com.devsuperior.dsmovieflix;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsMovieFlixApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsMovieFlixApplication.class, args);
+	}
+
+}
---
+++ backend/src/main/resources/import.sql
@@ -0,0 +1,2 @@
+//Comando para SEEDING(semear) o Banco H2 toda vez que reiniciar a aplicação
+
