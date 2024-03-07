c298e16706a58fbb6fa2cabb0eec65695207fc48
Clayton Pereira <clayton.pereira@outlook.com>
2024-03-06 23:06:02 -0300

    BackEnd Challenge MovieFlix - ORM of Entities Applied

--- backend/src/main/java/com/devsuperior/dsmovieflix/entities/Movie.java
+++ backend/src/main/java/com/devsuperior/dsmovieflix/entities/Movie.java
@@ -1,23 +1,42 @@
 package com.devsuperior.dsmovieflix.entities;
 
 import java.io.Serializable;
-import java.util.ArrayList;
-import java.util.List;
+import java.util.HashSet;
 import java.util.Objects;
+import java.util.Set;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.JoinColumn;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_movie")
 public class Movie implements Serializable {
 	private static final long serialVersionUID = 1L;
 	
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)	
 	private Long id;
 	private String title;
 	private String subTitle;
+	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
 	private Integer year;
 	private String imgUrl;
+	@Column(columnDefinition = "TEXT")
 	private String synopsis;
 	
+	@ManyToOne
+	@JoinColumn(name = "genre_id")
 	private Genre genre;
 	
-	private List<Review> reviews = new ArrayList<>();
+	@OneToMany(mappedBy = "movie")
+	private Set<Review> reviews = new HashSet<>();
 	
 	public Movie() {
 	}
@@ -88,7 +107,7 @@ public class Movie implements Serializable {
 		this.genre = genre;
 	}
 
-	public List<Review> getReviews() {
+	public Set<Review> getReviews() {
 		return reviews;
 	}
 
