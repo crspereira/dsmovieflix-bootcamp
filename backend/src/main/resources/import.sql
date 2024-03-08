9e6b34736c4a28c9fbeb7ae55a2c1bbb2a6d2e8b
Clayton Pereira <clayton.pereira@outlook.com>
2024-03-07 21:00:12 -0300

    BackEnd Challenge MovieFlix - H2 DataBase SEED Implemented

--- backend/src/main/resources/import.sql
+++ backend/src/main/resources/import.sql
@@ -1,3 +1,11 @@
 //Comando para SEEDING(semear) o Banco H2 toda vez que reiniciar a aplicação
 
+INSERT INTO tb_user (name, email, password) VALUES ('Bob Brown', 'bob@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+INSERT INTO tb_user (name, email, password) VALUES ('Ana Grey', 'ana@gmail.com', '$2a$10$eACCYoNOHEqXve8aIWT8Nu3PkMXWBaOxJ9aORUYzfMQCbVBIhZ8tG');
+
+INSERT INTO tb_role (authority) VALUES ('ROLE_VISITOR');
+INSERT INTO tb_role (authority) VALUES ('ROLE_MEMBER');
+
+INSERT INTO tb_user_roles (user_id, role_id) VALUES (1, 1);
+INSERT INTO tb_user_roles (user_id, role_id) VALUES (2, 2);
 
